c0ab69e4764f3cb68f39c436d36a6c3963f2981c
Roman Gaditskiy <51689121+gadirom@users.noreply.github.com>
2024-07-21 15:27:07 +0200

    added conditionals in the result builder

--- Sources/MetalBuilder/MetalResultBuilder/MetalResultBuilder.swift
+++ Sources/MetalBuilder/MetalResultBuilder/MetalResultBuilder.swift
@@ -3,9 +3,29 @@ import MetalKit
 
 @resultBuilder
 public enum MetalResultBuilder{
-    public static func buildBlock(_ components: MetalBuilderComponent...) -> MetalContent{
-        components
+    public static func buildBlock(_ components: MetalContent...) -> MetalContent{
+        components.flatMap{$0}
     }
+    
+    public static func buildEither(first component: MetalContent) -> MetalContent {
+           component
+       }
+       
+    public static func buildEither(second component: MetalContent) -> MetalContent {
+       component
+    }
+
+    public static func buildOptional(_ component: MetalContent?) -> MetalContent {
+        component ?? []
+    }
+    
+    public static func buildExpression(_ expression: MetalBuilderComponent) -> MetalContent {
+            [expression]
+    }
+        
+//    public static func buildExpression(_ expression: MetalContent) -> MetalBuilderComponent {
+//        expression
+//    }
 }
 
 public typealias MetalContent = [MetalBuilderComponent]
